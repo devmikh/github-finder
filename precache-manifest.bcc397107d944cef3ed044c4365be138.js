1db77289ca9dd6a59a87b6d8970d90dd7206f60c
Mikhail Serebriakov <mishacyb@gmail.com>
2019-09-16 08:25:52 -0700

    Updates

---
+++ precache-manifest.bcc397107d944cef3ed044c4365be138.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2146d5d419d16f2c0af200c4e1daa600",
+    "url": "/github-finder/index.html"
+  },
+  {
+    "revision": "c661ea41a723cd24794a",
+    "url": "/github-finder/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "810ece2ef6e0498f04c5",
+    "url": "/github-finder/static/js/2.ab410692.chunk.js"
+  },
+  {
+    "revision": "c661ea41a723cd24794a",
+    "url": "/github-finder/static/js/main.9de09bb5.chunk.js"
+  },
+  {
+    "revision": "7fe2b3be93711bb37430",
+    "url": "/github-finder/static/js/runtime~main.416793f8.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/github-finder/static/media/logo.5d5d9eef.svg"
+  }
+]);
